eb17528c974979859b9d59b612f5e43e9ee6b729
Johannes Wuensch <j.wuensch@protonmail.com>
2018-04-27 17:19:51 +0200

    press CONTROL to scroll horizontally

--- GUI/SortableTable/SortableTable.gd
+++ GUI/SortableTable/SortableTable.gd
@@ -6,11 +6,12 @@ extends ScrollContainer
 export (Array, String) var column_names
 export (int) var sort_column = 1
 
-
+onready var RowScrollContainer = $"VBoxContainer/RowScrollContainer"
 
 
 
 var previous_scroll_horizontal = 0
+var previous_scroll_vertical = 0
 
 
 
@@ -29,10 +30,14 @@ func _ready():
 func _on_SortableTable_gui_input(ev):
 			
 	if ev.is_action_pressed("ui_mouse_wheel_up_or_down"):
-		self.scroll_horizontal = previous_scroll_horizontal
+		if !Input.is_key_pressed(KEY_CONTROL):
+			self.scroll_horizontal = previous_scroll_horizontal
+		else:
+			RowScrollContainer.scroll_vertical = previous_scroll_vertical
 
 
 
 
 func _on_SortableTable_draw():
 	previous_scroll_horizontal = self.scroll_horizontal
+	previous_scroll_vertical = RowScrollContainer.scroll_vertical
