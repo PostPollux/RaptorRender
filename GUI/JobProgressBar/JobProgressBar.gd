33e83795c5efb214c7bfcd6b7fc616fcf257ab72
Johannes Wuensch <j.wuensch@protonmail.com>
2018-05-16 21:03:31 +0200

    resize progress bar with column

--- GUI/JobProgressBar/JobProgressBar.gd
+++ GUI/JobProgressBar/JobProgressBar.gd
@@ -5,6 +5,7 @@ onready var BarActive = $"ColorRect_active"
 onready var BarFinished = $"ColorRect_finished"
 onready var ProgressLabel = $"ProgressLabel"
 
+var CellContainer
 
 
 var chunks_total = 3
@@ -14,9 +15,19 @@ var chunks_active = 1
 
 func _ready():
 	
+	CellContainer = get_parent().get_parent()
+	CellContainer.connect("resized", self, "resize")
+	# set correct size
+	rect_min_size.x = CellContainer.rect_min_size.x - 5
+	
 	show_progress()
+	
 
-
+func resize():
+	# set correct size
+	rect_min_size.x = CellContainer.rect_min_size.x - 5
+	show_progress()
+	
 
 func set_chunks (total, finished, active):
 	chunks_total = total
@@ -28,7 +39,7 @@ func set_chunks (total, finished, active):
 func show_progress():
 	
 	
-	var total_bar_size = rect_min_size.x
+	var total_bar_size = rect_min_size.x - 4  # 4 because of margin left and right
 	
 	var bar_active_size = total_bar_size * (chunks_active + chunks_finished) / chunks_total 
 	
@@ -36,9 +47,9 @@ func show_progress():
 	
 	
 	
-	var bar_finished_size = total_bar_size * chunks_finished / chunks_total 
+	var bar_finished_size = float(total_bar_size) * float(chunks_finished) / float(chunks_total) 
 	
-	BarFinished.rect_min_size.x = bar_finished_size
+	BarFinished.rect_min_size.x = bar_finished_size 
 	
 	
 	ProgressLabel.text = String( int( float(chunks_finished) / float(chunks_total) * 100.0 ) ) + " %"
