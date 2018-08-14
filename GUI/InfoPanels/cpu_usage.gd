f3188ba730814e5876eae43081bf723e47d49a88
Johannes Wuensch <j.wuensch@protonmail.com>
2018-08-14 16:04:45 +0200

    tween  CpuUsageBar and MemoryUsageBar

--- GUI/InfoPanels/cpu_usage.gd
+++ GUI/InfoPanels/cpu_usage.gd
@@ -2,11 +2,17 @@ extends TextureProgress
 
 
 var client_id = ""
+var CpuUsageTween
 
 func _ready():
-	pass
+	CpuUsageTween = Tween.new()
+	self.add_child(CpuUsageTween)
+	
+
 
 func _process(delta):
 	
 	if client_id != "":
-		self.set_value( RaptorRender.rr_data.clients[client_id].cpu_usage ) 
+		CpuUsageTween.stop_all()
+		CpuUsageTween.interpolate_property(self,"value", self.get_value(), RaptorRender.rr_data.clients[client_id].cpu_usage, 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		CpuUsageTween.start() 
--- GUI/InfoPanels/memory_usage.gd
+++ GUI/InfoPanels/memory_usage.gd
@@ -2,11 +2,16 @@ extends TextureProgress
 
 
 var client_id = ""
+var MemoryUsageTween
 
 func _ready():
-	pass
+	MemoryUsageTween = Tween.new()
+	self.add_child(MemoryUsageTween)
+
 
 func _process(delta):
 	
 	if client_id != "":
-		self.set_value( RaptorRender.rr_data.clients[client_id].memory_usage ) 
+		MemoryUsageTween.stop_all()
+		MemoryUsageTween.interpolate_property(self,"value", self.get_value(), RaptorRender.rr_data.clients[client_id].memory_usage , 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		MemoryUsageTween.start() 
