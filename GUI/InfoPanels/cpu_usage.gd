13f4be66e97dfcc13c02b9fead4487041f5221eb
Johannes Wuensch <j.wuensch@protonmail.com>
2018-08-14 16:16:37 +0200

    do not tween when switching clients

--- GUI/InfoPanels/cpu_usage.gd
+++ GUI/InfoPanels/cpu_usage.gd
@@ -3,6 +3,7 @@ extends TextureProgress
 
 var client_id = ""
 var CpuUsageTween
+var last_shown_client
 
 func _ready():
 	CpuUsageTween = Tween.new()
@@ -13,6 +14,15 @@ func _ready():
 func _process(delta):
 	
 	if client_id != "":
-		CpuUsageTween.stop_all()
-		CpuUsageTween.interpolate_property(self,"value", self.get_value(), RaptorRender.rr_data.clients[client_id].cpu_usage, 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-		CpuUsageTween.start() 
+		
+		# smooth animate the value when we stay at this client
+		if last_shown_client == client_id:
+			CpuUsageTween.stop_all()
+			CpuUsageTween.interpolate_property(self,"value", self.get_value(), RaptorRender.rr_data.clients[client_id].cpu_usage, 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			CpuUsageTween.start()
+		
+		# immediately show the value when we just switched to this client from another one
+		else:
+			CpuUsageTween.stop_all()
+			self.set_value(RaptorRender.rr_data.clients[client_id].cpu_usage)
+			last_shown_client = client_id
--- GUI/InfoPanels/memory_usage.gd
+++ GUI/InfoPanels/memory_usage.gd
@@ -3,6 +3,7 @@ extends TextureProgress
 
 var client_id = ""
 var MemoryUsageTween
+var last_shown_client
 
 func _ready():
 	MemoryUsageTween = Tween.new()
@@ -12,6 +13,15 @@ func _ready():
 func _process(delta):
 	
 	if client_id != "":
-		MemoryUsageTween.stop_all()
-		MemoryUsageTween.interpolate_property(self,"value", self.get_value(), RaptorRender.rr_data.clients[client_id].memory_usage , 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-		MemoryUsageTween.start() 
+		
+		# smooth animate the value when we stay at this client
+		if last_shown_client == client_id:
+			MemoryUsageTween.stop_all()
+			MemoryUsageTween.interpolate_property(self,"value", self.get_value(), RaptorRender.rr_data.clients[client_id].memory_usage , 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+			MemoryUsageTween.start() 
+		
+		# immediately show the value when we just switched to this client from another one
+		else:
+			MemoryUsageTween.stop_all()
+			self.set_value(RaptorRender.rr_data.clients[client_id].memory_usage)
+			last_shown_client = client_id
