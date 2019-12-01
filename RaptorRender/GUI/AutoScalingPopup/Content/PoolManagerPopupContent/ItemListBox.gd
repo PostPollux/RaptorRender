6c9289699c08ca5193868ce0606ccc687c54374c
Johannes Wuensch <j.wuensch@protonmail.com>
2019-12-01 23:56:04 +0100

    refactor ItemListBox

--- RaptorRender/GUI/AutoScalingPopup/Content/PoolManagerPopupContent/ItemListBox.gd
+++ RaptorRender/GUI/AutoScalingPopup/Content/PoolManagerPopupContent/ItemListBox.gd
@@ -3,12 +3,14 @@ extends MarginContainer
 class_name ItemListBox
 
 ### PRELOAD RESOURCES
-var ListBoxItem_Base_res = preload("res://RaptorRender/GUI/AutoScalingPopup/Content/PoolManagerPopupContent/ListBoxItem_Base.tscn")
+var ItemListBoxItemRes = preload("res://RaptorRender/GUI/AutoScalingPopup/Content/PoolManagerPopupContent/ItemListBoxItem.tscn")
 
 
 ### SIGNALS
 signal item_selected
 signal selection_cleared
+signal item_name_changed
+signal item_doubleclicked
 
 
 ### ONREADY VARIABLES
@@ -20,6 +22,11 @@ onready var ItemVBox : VBoxContainer = $"ItemVBox"
 ### VARIABLES
 var SelectedItems : Array = []
 
+var item_names_editable : bool = false
+var items_dragable : bool = false
+var item_bg_color_normal : Color = Color(0.001, 0, 0, 1)
+var item_bg_color_selected : Color = Color(0.001, 0, 0, 1)
+
 
 
 
@@ -28,46 +35,79 @@ var SelectedItems : Array = []
 
 
 func _ready() -> void:
-	pass
+	
+	if item_bg_color_normal == Color(0.001, 0, 0, 1):
+		item_bg_color_normal = RRColorScheme.bg_1
+		
+	if item_bg_color_selected == Color(0.001, 0, 0, 1):
+		item_bg_color_selected = RRColorScheme.selected
 
 
 
 func clear() -> void:
 	for child in ItemVBox.get_children():
 		child.queue_free()
+	SelectedItems.clear()
 
 
 func clear_immediately() -> void:
 	for child in ItemVBox.get_children():
 		child.free()
+	SelectedItems.clear()
 
 
-func add_item(name : String, item_id : int) -> void:
-	var NewListBoxItem : ListBoxItem_Base = ListBoxItem_Base_res.instance()
+
+func add_item(name : String, item_id : int) -> int:
+	var NewListBoxItem : ItemListBoxItem = ItemListBoxItemRes.instance()
 	
 	NewListBoxItem.set_name(name)
 	NewListBoxItem.item_id = item_id
+	NewListBoxItem.name_editable = item_names_editable
+	NewListBoxItem.dragable = items_dragable
+	NewListBoxItem.color_normal = item_bg_color_normal
+	NewListBoxItem.color_selected = item_bg_color_selected
 	
 	NewListBoxItem.connect("item_clicked", self, "select_item")
+	NewListBoxItem.connect("item_doubleclicked", self, "item_doubleclicked")
 	NewListBoxItem.connect("select_all_pressed", self, "select_all")
-
+	NewListBoxItem.connect("drag_select", self, "drag_select_item")
+	NewListBoxItem.connect("name_changed", self, "item_name_changed")
+	
 	ItemVBox.add_child(NewListBoxItem)
+	
+	return NewListBoxItem.get_position_in_parent()
+
 
 
 func remove_item_by_id (item_id : int) -> void:
 	for child in ItemVBox.get_children():
 		if item_id == child.item_id:
+			SelectedItems.erase(child)
 			child.queue_free()
 
 
 func remove_item_by_name (item_name : String) -> void:
 	for child in ItemVBox.get_children():
 		if item_name == child.item_name:
+			SelectedItems.erase(child)
 			child.queue_free()
 
 
+func exit_and_apply_name_edit_mode_for_all_items(currently_clicked : Node) -> void:
+	
+	for child in ItemVBox.get_children():
+		
+		# don't exit on the clicked one itself. Otherwise we could not enter that mode with a double click at all...
+		if child != currently_clicked:
+			child.exit_and_apply_name_edit_mode()
 
 
+func item_doubleclicked(item_id : int) -> void:
+	emit_signal("item_doubleclicked", item_id)
+
+
+func item_name_changed(item_id : int) -> void:
+	emit_signal("item_name_changed", item_id)
 
 
 #############
@@ -76,6 +116,8 @@ func remove_item_by_name (item_name : String) -> void:
 
 
 func select_item( ClickedItem : Node):
+	
+	exit_and_apply_name_edit_mode_for_all_items(ClickedItem)
 	
 	if Input.is_key_pressed(KEY_CONTROL):
 		
@@ -102,13 +144,13 @@ func select_item( ClickedItem : Node):
 			
 			if last_selected_position < position_of_just_clicked_item:
 				for i in range(last_selected_position, position_of_just_clicked_item + 1, 1):
-					var SelectedItem : ListBoxItem_Base = ItemVBox.get_child(i)
+					var SelectedItem : ItemListBoxItem = ItemVBox.get_child(i)
 					SelectedItem.select()
 					if !SelectedItems.has(SelectedItem):
 						SelectedItems.append(SelectedItem)
 			else:
 				for i in range(last_selected_position, position_of_just_clicked_item -1, -1):
-					var SelectedItem : ListBoxItem_Base = ItemVBox.get_child(i)
+					var SelectedItem : ItemListBoxItem = ItemVBox.get_child(i)
 					SelectedItem.select()
 					if !SelectedItems.has(SelectedItem):
 						SelectedItems.append(SelectedItem)
@@ -123,6 +165,26 @@ func select_item( ClickedItem : Node):
 		SelectedItems.append(ClickedItem)
 		emit_signal("item_selected", ClickedItem.item_id)
 
+
+
+func drag_select_item(ClickedItem : ItemListBoxItem):
+	
+	if Input.is_key_pressed(KEY_CONTROL):
+		ClickedItem.deselect()
+		SelectedItems.erase(ClickedItem)
+		
+		
+	elif Input.is_key_pressed(KEY_SHIFT):
+		if ClickedItem.selected == false:
+			ClickedItem.select()
+			SelectedItems.append(ClickedItem)
+		
+	else:
+		clear_selection()
+		
+		ClickedItem.select()
+		SelectedItems.append(ClickedItem)
+		emit_signal("item_selected", ClickedItem.item_id)
 
 
 
---
+++ RaptorRender/GUI/AutoScalingPopup/Content/PoolManagerPopupContent/ItemListBoxItem.gd
@@ -0,0 +1,196 @@
+extends MarginContainer
+
+class_name ItemListBoxItem
+
+### PRELOAD RESOURCES
+
+### SIGNALS
+signal item_clicked
+signal item_doubleclicked
+signal select_all_pressed
+signal drag_select
+signal name_changed
+
+### ONREADY VARIABLES
+onready var BgColorRect : ColorRect = $"BgColorRect"
+onready var NameLabel : Label = $"MarginContainer/Label"
+onready var NameLineEdit : LineEdit = $"MarginContainer/NameEdit"
+
+### EXPORTED VARIABLES
+
+### VARIABLES
+var item_name : String = ""
+var item_id : int
+var selected : bool = false
+var hovered : bool = false
+
+var color_normal : Color = RRColorScheme.bg_1
+var color_selected : Color = RRColorScheme.selected
+
+var dragable : bool = false
+var name_editable : bool = false
+
+
+
+
+########## FUNCTIONS ##########
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if item_name != "":
+		NameLabel.text = item_name
+	DragManager.connect("drag_ended", self, "drag_just_ended")
+
+
+func _process(delta : float) -> void:
+	if hovered:
+		if Input.is_action_just_pressed("select_all"):
+			emit_signal("select_all_pressed")
+
+
+func set_name(name : String) -> void:
+	item_name = name
+	if is_instance_valid(NameLabel):
+		NameLabel.text = item_name
+
+
+
+func highlight() -> void:
+	if selected:
+		BgColorRect.color = color_selected.lightened(0.1)
+	else:
+		BgColorRect.color = color_normal.lightened(0.1)
+
+
+func remove_highlight() -> void:
+	if selected:
+		BgColorRect.color = color_selected
+	else:
+		BgColorRect.color = color_normal
+
+
+
+
+func select() -> void:
+	selected = true
+	BgColorRect.color = color_selected
+	
+	var mouse_pos : Vector2 = get_viewport().get_mouse_position()
+	if self.get_global_rect().has_point(mouse_pos):
+		BgColorRect.color = color_selected.lightened(0.1)
+
+
+
+func deselect() -> void:
+	selected = false
+	BgColorRect.color = color_normal
+
+
+func get_drag_data(_pos):
+	
+	if dragable:
+		DragManager.currently_dragging = true
+		
+		# generate a position corrected clone of self
+		var drag_preview : Control = Control.new()
+		var drag_clone : ItemListBoxItem = self.duplicate()
+		drag_clone.item_name = self.item_name
+		drag_clone.rect_position = -self.get_local_mouse_position()
+		drag_preview.add_child(drag_clone)
+		set_drag_preview(drag_preview)
+		
+		# hide the source node
+		self.modulate = Color(1, 1, 1, 0.3)
+		
+		# Return color as drag data
+		return self
+	
+	# Still return something. This is just a workaround. If we don't have this and we start to click and drag, mouse_entered and mouse_exited signals won't be executed on other nodes.
+	# with this added they get executed as expected!
+	else:
+		return "workaround"
+
+
+
+func can_drop_data(_pos, data):
+	return typeof(data) == TYPE_OBJECT
+
+
+
+func drop_data(_pos, data):
+	if dragable:
+		var desired_position : int = self.get_position_in_parent()
+		self.get_parent().move_child(data, desired_position)
+		data.modulate = Color(1, 1, 1, 1)
+
+
+func drag_just_ended() -> void:
+	if dragable:
+		self.modulate = Color(1, 1, 1, 1)
+
+
+
+func apply_name() -> void:
+	if name_editable:
+		NameLineEdit.visible = false
+		if NameLineEdit.text != "" and item_name != NameLineEdit.text:
+			item_name = NameLineEdit.text
+			NameLabel.text = item_name
+			emit_signal("name_changed", item_id)
+
+
+func enter_name_edit_mode() -> void:
+	if name_editable:
+		NameLineEdit.visible = true
+		NameLineEdit.text = item_name
+		NameLineEdit.select_all()
+		NameLineEdit.caret_blink = true
+		NameLineEdit.caret_position = NameLineEdit.text.length()
+		NameLineEdit.grab_focus()
+
+
+func exit_and_apply_name_edit_mode() -> void:
+	if name_editable:
+		if NameLineEdit.visible:
+			apply_name()
+
+
+
+
+func _on_Item_mouse_entered() -> void:
+	hovered = true
+	highlight()
+	if dragable == false:
+		if Input.is_action_pressed("ui_left_mouse_button"):
+			emit_signal("drag_select", self)
+
+
+func _on_Item_mouse_exited() -> void:
+	hovered = false
+	remove_highlight()
+
+
+func _on_Item_gui_input(event: InputEvent) -> void:
+	
+	# signal for doubleclick
+	if event is InputEventMouseButton and event.doubleclick:
+		emit_signal("item_doubleclicked")
+	
+	# signal for left clicked
+	if event.is_action_pressed("ui_left_mouse_button"):
+		emit_signal("item_clicked", self)
+
+
+
+func _on_NameEdit_text_entered(new_text: String) -> void:
+	apply_name()
+
+
+func _on_NameEdit_focus_exited() -> void:
+	apply_name()
+
+
+func _on_Item_item_doubleclicked() -> void:
+	if name_editable:
+		enter_name_edit_mode()
